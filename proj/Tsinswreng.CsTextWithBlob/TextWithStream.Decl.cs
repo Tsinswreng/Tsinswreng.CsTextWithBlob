f7b1ac84be0d7018f4960b885e45d9136eac0bac
Tsinswreng <Tsinswreng@gmail.com>
2026-04-11 20:34:52 +0800

    wip:製 TextWithStream

---
+++ proj/Tsinswreng.CsTextWithBlob/TextWithStream.Decl.cs
@@ -0,0 +1,31 @@
+using Tsinswreng.CsCore;
+
+namespace Tsinswreng.CsTextWithBlob;
+
+
+[Doc(@$"
+- {nameof(HeaderBytesLen)}: 8 bytes head (big endian, store the bytes count of {nameof(Text)})
+- {nameof(Text)}
+- blob
+")]
+public interface ITextWithStream{
+	
+	public u64 HeaderBytesLen{get;set;}
+	public string Text { get;set;}
+	public Stream Payload { get;set;}
+}
+
+
+public partial class TextWithStream{
+	public u64 HeaderBytesLen{get;set;}
+	public string Text {get;set;}
+	public Stream Payload {get;set;}
+	public partial TextWithStream();
+	public static partial TextWithStream Pack(
+		u64 HeaderBytesLen,
+		string Text,
+		Stream Payload
+	);
+	public static partial TextWithStream Unpack(Stream stream);
+	
+}
